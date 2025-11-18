84753652fef019e2501e21fc47336f52c6067c18
ry726 <ry@dummy.com>
2025-11-19 06:59:42 +0700

    feat: Add perpustakaan table * Add perpustakaan table to database

---
+++ database/migrations/2025_11_18_235730_create_perpustakaan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('perpustakaans', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover_buku')->nullable();
+            $table->string('nama_buku');
+            $table->enum('ketersediaan', ['tersedia', 'tidak tersedia'])->default('tersedia');
+            $table->string('author')->nullable();
+            $table->string('genre_buku')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('perpustakaans');
+    }
+};
